1bbf2fca591116837830228201ebb27f2770a398
Alemaite <95234399+Alemaite@users.noreply.github.com>
2023-03-27 11:31:55 +0200

    Delete angular/src directory

--- angular/src/app/models/ingredients.ts
+++
@@ -1,9 +0,0 @@
-import { IngredientModel } from './ingredient';
-
-export class IngredientsModel {
-  constructor(
-    public title: string,
-    public ingredients: IngredientModel[],
-    public desc?: string
-  ) {}
-}
--- angular/src/app/models/shopping-list.ts
+++
@@ -1,5 +0,0 @@
-import { IngredientsModel } from './ingredients';
-
-export class ShoppingListModel {
-  constructor(public userId: string, public items: IngredientsModel[]) {}
-}
