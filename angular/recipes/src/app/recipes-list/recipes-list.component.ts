94a927ba42ded17a1a38a6e95a2c418ae25dfdab
Alemaite <95234399+Alemaite@users.noreply.github.com>
2023-04-07 10:41:28 +0200

    Now Responsive

--- angular/recipes/src/app/recipes-list/recipes-list.component.ts
+++ angular/recipes/src/app/recipes-list/recipes-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+  styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent implements OnInit {
-
-  constructor() { }
+  handsetMode = false;
+  constructor(private responsive: BreakpointObserver) {}
 
   ngOnInit(): void {
+    this.responsive
+      .observe([Breakpoints.HandsetLandscape, Breakpoints.HandsetPortrait])
+      .subscribe((result) => {
+        if (result.matches) {
+          this.handsetMode = true;
+          return;
+        }
+        this.handsetMode = false;
+        return;
+      });
+    console.log(this.handsetMode);
   }
-
 }
