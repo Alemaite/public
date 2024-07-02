445c9b2dcc0230e5391d54b82a4734af46d18e7c
Alemaite <95234399+Alemaite@users.noreply.github.com>
2024-07-02 21:16:55 +0200

    Delete angular/time-tracking directory

--- angular/time-tracking/backend/models/session.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const sessionSchema = mongoose.Schema({
-  _id: { type: String },
-  expires: { type: String },
-  session: { type: { activities: { type: [Object] } } },
-});
-
-module.exports = mongoose.model("Session", sessionSchema);
