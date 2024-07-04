916553013ab4b5d35d0c3b7c8d1e6b71cf8d9625
Alemaite <95234399+Alemaite@users.noreply.github.com>
2024-07-04 09:03:35 +0200

    Add files via upload

--- recipes-frontend/src/app/app.component.ts
+++ recipes-frontend/src/app/app.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  handsetMode: boolean;
+  landscapeMode: boolean;
+
+  constructor(private responsive: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.responsive
+      .observe([Breakpoints.HandsetPortrait, Breakpoints.TabletPortrait])
+      .pipe(untilDestroyed(this))
+      .subscribe((result) => {
+        if (result.matches) {
+          this.handsetMode = true;
+          this.landscapeMode = false;
+          return;
+        }
+        this.handsetMode = false;
+      });
+    this.responsive
+      .observe([Breakpoints.HandsetLandscape, Breakpoints.TabletLandscape])
+      .pipe(untilDestroyed(this))
+      .subscribe((result) => {
+        if (result.matches) {
+          this.handsetMode = true;
+          this.landscapeMode = true;
+          return;
+        }
+        this.landscapeMode = false;
+      });
+  }
+}
