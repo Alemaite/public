b0dfb0df64e83d50851fa7bd8ea7454bf53f013e
Alemaite <95234399+Alemaite@users.noreply.github.com>
2024-08-16 16:20:42 +0200

    Add files via upload

--- recipes-frontend/src/environments/environment.prod.ts
+++ recipes-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://iu-recipes-service-f685138f7469.herokuapp.com'
+  apiUrl: 'https://iu-recipes-service-f685138f7469.herokuapp.com',
+  localUrl: 'https://iu-recipes-frontend-997a604138d2.herokuapp.com',
 };
--- recipes-frontend/src/environments/environment.ts
+++ recipes-frontend/src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
+  localUrl: 'http://localhost:4200',
 };
 
 /*
